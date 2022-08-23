e3d7526871422734595c872d272e485e21a8d0ad
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-23 16:57:15 +0300

    задача38. найти min и max массива вещественных чисел

--- Z38/Program.cs
+++ Z38/Program.cs
@@ -4,22 +4,58 @@
 
 //[3 7 22 2 78] -> 76
 
-            // объявляем переменные и вводим данные
+//Числа с плавающей точкой (или действительные числа) представлены типами float и double.
+//Используются для хранения значений с точностью до определенного знака после десятичной точки.
+//double — это числа с двойной точностью, максимально приближённые к заданным или полученным
+//в результате вычислений значениям. Используется в Java для любых математических вычислений
+//(квадратный корень, синус, косинус.). float — менее точный тип с плавающей точкой.
+// различия в объёме памяти float (32 бита), double (64 бита).
 
-double[] array = new double[5]; // количество чисел в массиве
-Console.WriteLine("Enter the array elements:"); // заполняем массив с клавиатуры
-for (int i = 0; i < array.Length; i++)
-    array[i] = double.Parse(Console.ReadLine());
-double max = array[0];// переменная макс
-double min = array[0];// переменная минимум
-for (int i = 1; i < array.Length; i++) // проходимся в цикле по массиву
+            // объявляем переменные и вводим данные
+/*
+double[] arrayay = new double[5]; // количество чисел в массиве
+Console.WriteLine("Enter the arrayay elements:"); // заполняем массив с клавиатуры
+for (int i = 0; i < arrayay.Length; i++)
+{
+    arrayay[i] = double.Parse(Console.ReadLine());
+}
+double max = arrayay[0];// переменная макс
+double min = arrayay[0];// переменная минимум
+for (int i = 1; i < arrayay.Length; i++) // проходимся в цикле по массиву
     { 
-    if (array[i] < min) // если нашли меньше элемент чем был, то запоминаем его
-       min = array[i];
-    if (array[i] > max) // если нашли больше элемент чем был, то запоминаем его
-       max = array[i];
+    if (arrayay[i] < min) // если нашли меньше элемент чем был, то запоминаем его
+       min = arrayay[i];
+    if (arrayay[i] > max) // если нашли больше элемент чем был, то запоминаем его
+       max = arrayay[i];
     }
 Console.WriteLine();
 Console.WriteLine("Difference between max and min elements = " + (max - min)); // вывод результата
+*/
+//Привет Миша! Проблемы с решением этой задачи:
+//1) Если здесь решение в цикле while? У меня есть сомнения, т.к. i = 0 и i = 1.
+//Можно ли выйти из этого положения?
 
-
+double[] array;
+double max, min;
+int N;
+Random r = new Random();
+Console.WriteLine("Enter the dimension of the array:");
+N = int.Parse(Console.ReadLine().ToString());
+array = new double[N];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = r.Next(0, 50);
+    Console.Write(array[i] + " ");
+}
+            
+    max = array[0];
+    min = array[0];
+            
+for (int i = 1; i < array.Length; i++)
+    {
+    if (array[i] > max)
+        max = array[i];
+    if (array[i] < min)
+        min = array[i];
+    }
+Console.WriteLine($"\nMax = {max}\nMin = {min}\nDifference = {max - min}");
